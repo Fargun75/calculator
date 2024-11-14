ac0fb5819969f5b36e1fc7413e13d6949ca3d7f1
Gunnar Farup <113795596+Fargun75@users.noreply.github.com>
2024-11-14 09:03:10 +0100

    Add basic math functions

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,28 @@
-console.log('Det virker!')
+function add(a,b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(operat, num1, num2) {
+    if(operat == '+') return add(num1, num2);
+    if(operat == '-') return subtract(num1, num2);
+    if(operat == '*') return multiply(num1, num2);
+    if(operat == '/') return divide(num1, num2);
+}
+
+console.log(operate('/', 4, 4));
