3a05a61aa169917ed337f9f744699ae7f2106d8b
Gunnar Farup <113795596+Fargun75@users.noreply.github.com>
2024-11-15 19:55:42 +0100

    Create a designated populateDisplay function

--- JS/script.js
+++ JS/script.js
@@ -29,18 +29,40 @@ const display = document.querySelector('#display');
 display.textContent = '0';
 let number = [];
 
+function populateDisplay(num) {
+    const display = document.querySelector('#display');
+    display.textContent = num;
+
+}
 const numberButtons = document.querySelectorAll('.number');
 numberButtons.forEach((button) => button.addEventListener('click', (e) => {
     number.push(e.target.id);
-    firstNumber = parseInt(number.join(''));
-    display.textContent = firstNumber;
+    newNumber = parseInt(number.join(''));
+    populateDisplay(newNumber);
 }));
+
 
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach((button) => button.addEventListener('click', (e) => {
     if(!firstNumber) {
+        firstNumber = parseFloat(number.join(''));
+        number = [];
+        return firstNumber;
+    } else if(!secondNumber) {
+        secondNumber = parseFloat(number.join(''));
+        number = [];
+        return secondNumber;
     }
     console.log(firstNumber);
+    console.log(secondNumber);
+    //operator = e.target.id;
+    //console.log(operator);
+    number = [];
+    console.log(number);
 }));
+
+
+
+
 
 
